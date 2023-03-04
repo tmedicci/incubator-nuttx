9d820b0b00e475f71123dec7101616dbcb469600
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2023-03-04 12:35:00 -0300

    net/igmp/igmp_join: ignore if address is already in group

--- net/igmp/igmp_join.c
+++ net/igmp/igmp_join.c
@@ -160,12 +160,9 @@ int igmp_joingroup(struct net_driver_s *dev,
       /* Add the group (MAC) address to the ether drivers MAC filter list */
 
       igmp_addmcastmac(dev, (FAR in_addr_t *)&grpaddr->s_addr);
-      return OK;
     }
 
-  /* Return EEXIST if the address is already a member of the group */
-
-  return -EEXIST;
+  return OK;
 }
 
 #endif /* CONFIG_NET_IGMP */
